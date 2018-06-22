35cf6abdb33becade24ba51d749a039e4fad9235
acelyc111 <405403881@qq.com>
2018-06-22 18:32:54 +0800

    test

--- src/geo/sample/sample.cpp
+++ src/geo/sample/sample.cpp
@@ -7,71 +7,95 @@
 #include <iostream>
 #include <s2/s2testing.h>
 #include <s2/s2cell.h>
+#include "monitoring/histogram.h"
 #include <dsn/utility/strings.h>
+#include <rocksdb/env.h>
+#include <dsn/utility/string_conv.h>
 
 static const int data_count = 10000;
-static const int test_count = 1;
-static const double radius = 5000.0;
 
 int main(int argc, char **argv)
 {
-    if (argc != 4) {
-        std::cerr << "USAGE: " << argv[0] << "<cluster-name> <app-name> <geo-app-name>" << std::endl;
+    if (argc != 6) {
+        std::cerr << "USAGE: " << argv[0] << "<cluster-name> <app-name> <geo-app-name>"
+                  << std::endl;
         return -1;
     }
 
-    pegasus::geo::geo_client my_geo(
-        "config.ini", argv[1], argv[2], argv[3], new pegasus::geo::latlng_extractor_for_lbs());
+    std::string cluster_name = argv[1];
+    std::string app_name = argv[2];
+    std::string geo_app_name = argv[3];
+    double radius = 0.0;
+    if (!dsn::buf2double(argv[4], radius)) {
+        std::cerr << "radius is invalid: " << argv[4] << std::endl;
+        return -1;
+    }
+    int test_count = 2000;
+    if (!dsn::buf2int32(argv[5], test_count)) {
+        std::cerr << "test_count is invalid: " << argv[5] << std::endl;
+        return -1;
+    }
+    pegasus::geo::geo_client my_geo("config.ini",
+                                    cluster_name.c_str(),
+                                    app_name.c_str(),
+                                    geo_app_name.c_str(),
+                                    new pegasus::geo::latlng_extractor_for_lbs());
 
     // cover beijing 5th ring road
     S2LatLngRect rect(S2LatLng::FromDegrees(39.810151, 116.194511),
                       S2LatLng::FromDegrees(40.028697, 116.535087));
 
     // generate data for test
-    for (int i = 0; i < data_count; ++i) {
-        S2LatLng latlng(S2Testing::SamplePoint(rect));
-        std::string id = std::to_string(i);
-        std::string value = id + "|2018-06-05 12:00:00|2018-06-05 13:00:00|abcdefg|" +
-                            std::to_string(latlng.lng().degrees()) + "|" +
-                            std::to_string(latlng.lat().degrees()) + "|123.456|456.789|0|-1";
+    //    for (int i = 0; i < data_count; ++i) {
+    //        S2LatLng latlng(S2Testing::SamplePoint(rect));
+    //        std::string id = std::to_string(i);
+    //        std::string value = id + "|2018-06-05 12:00:00|2018-06-05 13:00:00|abcdefg|" +
+    //                            std::to_string(latlng.lng().degrees()) + "|" +
+    //                            std::to_string(latlng.lat().degrees()) + "|123.456|456.789|0|-1";
+    //
+    //        int ret = my_geo.set(id, "", value, 1000);
+    //        if (ret != pegasus::PERR_OK) {
+    //            std::cerr << "set data failed. error=" << ret << std::endl;
+    //        }
+    //    }
 
-        int ret = my_geo.set(id, "", value, 1000);
-        if (ret != pegasus::PERR_OK) {
-            std::cerr << "set data failed. error=" << ret << std::endl;
-        }
-    }
-
+    rocksdb::HistogramImpl hist;
+    rocksdb::Env *env = rocksdb::Env::Default();
     // test search_radial by lat & lng
     for (int i = 0; i < test_count; ++i) {
         S2LatLng latlng(S2Testing::SamplePoint(rect));
 
+        uint64_t start_nanos = env->NowNanos();
         std::list<pegasus::geo::SearchResult> result;
         my_geo.search_radial(latlng.lat().degrees(),
                              latlng.lng().degrees(),
                              radius,
                              -1,
-                             pegasus::geo::geo_client::SortType::random,
+                             pegasus::geo::geo_client::SortType::asc,
                              500,
                              result);
+        hist.Add(env->NowNanos() - start_nanos);
 
         std::cout << "count: " << result.size() << std::endl;
-        for (auto &data : result) {
-            std::cout << data.to_string() << std::endl;
-        }
+        //        for (auto &data : result) {
+        //            std::cout << data.to_string() << std::endl;
+        //        }
     }
 
-    // test search_radial by key
-    for (int i = 0; i < test_count; ++i) {
-        std::string id = std::to_string(i);
-        std::list<pegasus::geo::SearchResult> result;
-        my_geo.search_radial(
-            id, "", radius, 20, pegasus::geo::geo_client::SortType::asc, 500, result);
+    std::cout << hist.ToString() << std::endl;
 
-        std::cout << "count: " << result.size() << std::endl;
-        for (auto &data : result) {
-            std::cout << data.to_string() << std::endl;
-        }
-    }
+    //    // test search_radial by key
+    //    for (int i = 0; i < test_count; ++i) {
+    //        std::string id = std::to_string(i);
+    //        std::list<pegasus::geo::SearchResult> result;
+    //        my_geo.search_radial(
+    //            id, "", radius, 20, pegasus::geo::geo_client::SortType::asc, 500, result);
+    //
+    //        std::cout << "count: " << result.size() << std::endl;
+    //        for (auto &data : result) {
+    //            std::cout << data.to_string() << std::endl;
+    //        }
+    //    }
 
     return 0;
 }
