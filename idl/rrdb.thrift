67bc71ed642f0e938d32bf5c41cdd2350979a49e
Yingchun Lai <laiyingchun@apache.org>
2023-01-13 17:07:51 +0800

    init

--- idl/rrdb.thrift
+++ idl/rrdb.thrift
@@ -323,7 +323,7 @@ service rrdb
     oneway void clear_scanner(1:i64 context_id);
 }
 
-// ONLY FOR GO & JAVA
+// ONLY FOR GO & JAVA & Python
 service meta
 {
     dsn.layer2.query_cfg_response query_cfg(1:dsn.layer2.query_cfg_request query);
